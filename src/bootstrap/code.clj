423ae58eb3625d6b7cde952657a3b612a3318988
Micha Niskin <asdf@ubergibson.com>
2013-12-11 13:43:22 -0500

    work

--- src/bootstrap/code.clj
+++ src/bootstrap/code.clj
@@ -12,10 +12,15 @@
         :out parse-snip)))
 
 (defn do-highlight [lexer body]
-  (or (do-pygments lexer body)
+  (or (and (not= "none" lexer) (do-pygments lexer body))
       `(tailrecursion.hoplon/pre (tailrecursion.hoplon/code ~body))))
+
+(defmacro highlight
+  [lang body]
+  (do-highlight (name lang) body))
 
 (defmacro clj   [body] (do-highlight "clj"  body))
 (defmacro html  [body] (do-highlight "html" body))
 (defmacro xml   [body] (do-highlight "xml"  body))
+(defmacro bash  [body] (do-highlight "bash" body))
 
