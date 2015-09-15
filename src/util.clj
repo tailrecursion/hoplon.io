b83b1f3aab0e03116b432b8fc001dca7bb371f8c
Alan Dipert <alan@dipert.org>
2015-09-15 00:32:49 -0400

    demos

--- src/util.clj
+++ src/util.clj
@@ -1,6 +1,22 @@
 (ns util
+  (:import org.python.util.PythonInterpreter)
   (:require [markdown.core    :as md]
             [clojure.data.xml :as xml]))
+
+(defn pygmentize [file-ext code]
+  (try
+    (-> (doto (PythonInterpreter.)
+          (.set  "code" code)
+          (.set  "file" (str "test." file-ext))
+          (.exec "from pygments import highlight\n")
+          (.exec "from pygments.lexers import guess_lexer_for_filename\n")
+          (.exec "from pygments.formatters import HtmlFormatter\n")
+          (.exec "lex = guess_lexer_for_filename(file, code)\n")
+          (.exec "fmt = HtmlFormatter(noclasses=True)\n")
+          (.exec "result = highlight(code, lex, fmt)"))
+        (.get "result" java.lang.String))
+    (catch Throwable t
+      (throw (ex-info "pygments exception" {:file-ext file-ext :code code} t)))))
 
 (defn xml->hoplon
   [{:keys [tag attrs content] :as elem}]
@@ -15,3 +31,8 @@
         xml/parse-str
         xml->hoplon
         ((fn [[_ & kids]] `(~wrapper-sym ~@kids))))))
+
+(defmacro highlight [file-ext code]
+  (->> (pygmentize (name file-ext) code)
+       xml/parse-str
+       xml->hoplon))
